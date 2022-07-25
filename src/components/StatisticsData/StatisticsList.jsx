f232817eebaa4a1f0401201e073f26e7a49b6fab
Yuliia Kiryushina <yulio1999@gmail.com>
2022-07-25 17:14:29 +0300

    fix title

--- src/components/StatisticsData/StatisticsList.jsx
+++ src/components/StatisticsData/StatisticsList.jsx
@@ -5,7 +5,7 @@ import { Item } from '../StatisticsData/StatisticsItem';
 export const StatisticsList = ({ items, title }) => {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{ title }</h2>
+      {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.list}>
         {items.map(({ id, label, percentage }) => (
           <Item key={id} label={label} percentage={percentage} />
